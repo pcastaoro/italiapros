fbb693b9ec2102888b543b41734b2c0e6f562e5c
pietrocastaoro <pcastaoro@gmail.com>
2025-01-25 12:49:04 +0100

    feat: new table compo

--- App-web/frontend/src/pages/LolPage.jsx
+++ App-web/frontend/src/pages/LolPage.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import '../styles/lolpage.css';
+import CustomizedTables from "../components/Table";
 
 const LolPage = () => {
     return (
-        <div className="button-container">
-            <a href="https://lol.fandom.com/wiki/League_of_Legends_Italian_Tournament" target="_blank"
-               rel="noopener noreferrer">
-                <button className="lol-button">LIT</button>
-            </a>
-            <a href="https://lol.fandom.com/wiki/Blooming_Talents_League" target="_blank" rel="noopener noreferrer">
-                <button className="lol-button">BTL</button>
-            </a>
-            <a href="https://lol.fandom.com/wiki/Circuito_Tormenta_Italy" target="_blank" rel="noopener noreferrer">
-                <button className="lol-button">TORMENTA</button>
-            </a>
+        <div className="lolpage-container">
+            <div className="button-container">
+                <a href="https://lol.fandom.com/wiki/League_of_Legends_Italian_Tournament" target="_blank"
+                   rel="noopener noreferrer">
+                    <button className="lol-button">LIT</button>
+                </a>
+                <a href="https://lol.fandom.com/wiki/Blooming_Talents_League" target="_blank" rel="noopener noreferrer">
+                    <button className="lol-button">BTL</button>
+                </a>
+                <a href="https://lol.fandom.com/wiki/Circuito_Tormenta_Italy" target="_blank" rel="noopener noreferrer">
+                    <button className="lol-button">TORMENTA</button>
+                </a>
+            </div>
+            <div className="table-container">
+                <CustomizedTables/> {/* Aggiunge la tabella */}
+            </div>
         </div>
     );
 };
