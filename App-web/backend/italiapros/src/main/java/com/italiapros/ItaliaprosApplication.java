4b3a0078c204b8891ae5a051519f34db456853d6
pietrocastaoro <pcastaoro@gmail.com>
2025-02-11 11:13:14 +0100

    feat: init backend

---
+++ App-web/backend/italiapros/src/main/java/com/italiapros/ItaliaprosApplication.java
@@ -0,0 +1,13 @@
+package com.italiapros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaliaprosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItaliaprosApplication.class, args);
+    }
+
+}
---
+++ App-web/backend/italiapros/src/test/java/com/italiapros/ItaliaprosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italiapros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItaliaprosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
